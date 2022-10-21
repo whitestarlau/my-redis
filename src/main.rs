82ba3c67d075387efb570e7b56124553bc58972f
liuyixin.star <liuyixin.star@bytedance.com>
2022-10-21 17:23:17 +0800

    shared state.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use bytes::Bytes;
 use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
@@ -7,24 +12,26 @@ async fn main() {
     // 监听指定地址，等待 TCP 连接进来
     let listener = TcpListener::bind("127.0.0.1:10176").await.unwrap();
 
+    let db: Db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // 第二个被忽略的项中包含有新连接的 `IP` 和端口信息
         let (socket, _) = listener.accept().await.unwrap();
         // 为每一条连接都生成一个新的任务，
         // `socket` 的所有权将被移动到新的任务中，并在那里进行处理
 
+        let dbClone = db.clone();
+
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, dbClone).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
     use std::collections::HashMap;
 
-    // 使用 hashmap 来存储 redis 的数据
-    let mut db = HashMap::new();
 
     // `mini-redis` 提供的便利函数，使用返回的 `connection` 可以用于从 socket 中读取数据并解析为数据帧
     let mut connection = Connection::new(socket);
@@ -34,11 +41,15 @@ async fn process(socket: TcpStream) {
         println!("frame: {:?}", frame);
         let response: Frame = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
+                let mut db = db.lock().unwrap();
+
                 // 值被存储为 `Vec<u8>` 的形式
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
+
                 if let Some(value) = db.get(cmd.key()) {
                     // `Frame::Bulk` 期待数据的类型是 `Bytes`， 该类型会在后面章节讲解，
                     // 此时，你只要知道 `&Vec<u8>` 可以使用 `into()` 方法转换成 `Bytes` 类型
